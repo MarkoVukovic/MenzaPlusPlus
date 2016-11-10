173eba0c06bb7d21109bbcb29fe119ecc5a71440
MarkoVukovic <marko.vukovic95@gmail.com>
2016-11-10 21:35:05 +0100

    INITIAL

---
+++ MenzaPlusPlus/MenzaPlusPlus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MenzaPlusPlus.Startup))]
+namespace MenzaPlusPlus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
